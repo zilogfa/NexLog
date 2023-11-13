a1870712c22ba12c4f472a5d0a00e7ba7a4a0fd6
zilogfa <Zilogfa@live.com>
2023-11-13 15:44:42 -0800

    Blog UI Dev I/P Fav posts added.

--- app/routes/blog_routes.py
+++ app/routes/blog_routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request
 from flask import Blueprint
 from flask_login import login_required, current_user
 from app import db
-from app.models import Blog, Post
+from app.models import Blog, Post, Subject
 from . import auth_routes
 
 
@@ -10,9 +10,12 @@ blog_routes = Blueprint('blog', __name__, subdomain='<user_subdomain>')
 
 @blog_routes.route('/')
 def blog(user_subdomain):
-    posts = Post.query.order_by(Post.created_at.desc()).all()
-    blog = Blog.query.all()
-    return render_template('blog.html', subdomain=user_subdomain, posts=posts, blog=blog)
+    blog = Blog.query.filter_by(subdomain=user_subdomain).first()
+    posts = Post.query.filter_by(blog_id=blog.id).order_by(Post.created_at.desc()).all()
+    subjects = Subject.query.all()
+    top_posts = Post.query.filter_by(blog_id=blog.id).order_by(Post.views.desc()).limit(5).all()
+    
+    return render_template('blog.html', posts=posts, blog=blog, subjects=subjects, top_posts=top_posts)
 
 # @blog_routes.route('/admin_dashboard')
 # @login_required
@@ -31,3 +34,9 @@ def blog(user_subdomain):
 def view_post():
     pass
 
+
+
+
+
+# Top Posts from all posts.
+# top_posts = Post.query.order_by(Post.views.desc()).limit(5).all()
