7294eb0984e7df2421b44ce8270acacb1fa7c4ca
zilogfa <Zilogfa@live.com>
2023-11-30 15:44:54 -0800

    About Section added. Ready to add contents.

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -12,3 +12,8 @@ TITLE = "NexLog"
 @main_routes.route('/')
 def main():
     return render_template('main/main.html', title=TITLE)
+
+
+@main_routes.route('/about')
+def about():
+    return render_template('main/about.html', title=TITLE)
