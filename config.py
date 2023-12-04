f7080ec1ae22dbe30d172cb6252fc5ff50dae15b
zilogfa <Zilogfa@live.com>
2023-12-03 23:58:41 -0800

    Config: SERVERNAME commented out

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static'
-    SERVER_NAME = 'localhost:8181'
-    SESSION_COOKIE_DOMAIN = '.localhost'
-    REMEMBER_COOKIE_DOMAIN = '.localhost'
+    # SERVER_NAME = 'localhost:8181'
+    # SESSION_COOKIE_DOMAIN = '.localhost'
+    # REMEMBER_COOKIE_DOMAIN = '.localhost'
     WTF_CSRF_ENABLED = False
