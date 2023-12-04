a3e3767915597ded0a70cb7324b6dde0be779c74
zilogfa <Zilogfa@live.com>
2023-12-04 09:39:26 -0800

    SERVER NAME

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
+import os
+
 class Config:
     SECRET_KEY = 'secret_key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static'
-    SERVER_NAME = 'nexlog.us'
-    SERVER_NAME = 'nexlog-c4d6a2de71db.herokuapp.com'
+    WTF_CSRF_ENABLED = False
+    if os.environ.get('FLASK_ENV') == 'production':
+        SERVER_NAME = 'nexlog.us'
+    else:
+        SERVER_NAME = 'nexlog-c4d6a2de71db.herokuapp.com'
     # SESSION_COOKIE_DOMAIN = '.nexlog.us'
     # REMEMBER_COOKIE_DOMAIN = '.nexlog.us'
-    WTF_CSRF_ENABLED = False
+    
