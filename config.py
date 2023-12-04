cbcdc0e391b8708c50d5a0f17c10f70505683e3c
zilogfa <Zilogfa@live.com>
2023-12-04 10:13:26 -0800

    No Server_name

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static'
     WTF_CSRF_ENABLED = False
-    if os.environ.get('FLASK_ENV') == 'production':
-        SERVER_NAME = 'nexlog.us'
-    else:
-        SERVER_NAME = 'nexlog-c4d6a2de71db.herokuapp.com'
+    # if os.environ.get('FLASK_ENV') == 'production':
+    #     SERVER_NAME = 'nexlog.us'
+    # else:
+    #     SERVER_NAME = 'nexlog-c4d6a2de71db.herokuapp.com'
     # SESSION_COOKIE_DOMAIN = '.nexlog.us'
     # REMEMBER_COOKIE_DOMAIN = '.nexlog.us'
     
