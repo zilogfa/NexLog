871c4afce307776463d839abb7d56be6979930a2
zilogfa <Zilogfa@live.com>
2023-12-05 11:05:40 -0800

    SERVER_NAME: 'nexblog.us' added

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SERVER_NAME = 'nexblog.us'
     SECRET_KEY = 'secret_key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
